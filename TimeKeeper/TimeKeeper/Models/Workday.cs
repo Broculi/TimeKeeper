f1a00f9714049ce7660e5ac8837750e9f2f57de5
Christian Brummer <brummer.c@web.de>
2018-01-27 13:04:40 +0100

    initial commit

---
+++ TimeKeeper/TimeKeeper/Models/Workday.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeKeeper.Models
+{
+    public class Workday
+    {
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public TimeSpan WorkTime => (StartTime > EndTime) ? TimeSpan.Zero : EndTime.Subtract(StartTime);
+    }
+}
---
+++ TimeKeeper/TimeKeeper/Services/Locator.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeKeeper.Services
+{
+    public class Locator
+    {
+        static Locator()
+        {
+            //SimpleIoc.Default.Register<MainViewModel>();
+        }
+    }
+}
